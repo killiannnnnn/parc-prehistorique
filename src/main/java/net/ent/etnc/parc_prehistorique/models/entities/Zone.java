3a1c7f96091d75db3b05b09293665aa14d04659d
killiannnnnn <k.muscat@outlook.fr>
2026-08-28 08:48:35 +0200

    Update Zone entity

--- src/main/java/net/ent/etnc/parc_prehistorique/models/entities/Zone.java
+++ src/main/java/net/ent/etnc/parc_prehistorique/models/entities/Zone.java
@@ -1,13 +1,58 @@
 package net.ent.etnc.parc_prehistorique.models.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import net.ent.etnc.parc_prehistorique.models.commons.AbstractPersistableWithIdSetter;
 import net.ent.etnc.parc_prehistorique.models.enums.Enclo;
 import net.ent.etnc.parc_prehistorique.models.enums.EtatEnclo;
 
-public class Zone {
+@Entity
+@Table(
+        name = "zones"
+)
+@ToString(of = {"nom", "enclo", "capaciteMax", "etat", "description"}, callSuper = true)
+@EqualsAndHashCode(callSuper = false)
+public class Zone extends AbstractPersistableWithIdSetter<Long> {
+
+    @Getter
+    @Setter
+    @NotEmpty
+    @Column(name = "nom", nullable = false, length = 50)
     private String nom;
+
+    @Getter
+    @Setter
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "enclo", nullable = false, length = 50)
     private Enclo enclo;
+
+    @Getter
+    @Setter
+    @NotNull
+    @Positive
+    @Max(20)
+    @Column(name = "capacite_max", nullable = false)
     private int capaciteMax;
+
+    @Getter
+    @Setter
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "etat", nullable = false, length = 50)
     private EtatEnclo etat;
+
+    @Getter
+    @Setter
+    @NotEmpty
+    @Column(name = "description", nullable = false, length = 50)
     private String description;
 
 }
