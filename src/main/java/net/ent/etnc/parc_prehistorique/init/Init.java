01edf1a513be636d2d88f171e426b53fbdf658db
killiannnnnn <k.muscat@outlook.fr>
2026-08-28 10:52:40 +0200

    Update Init

--- src/main/java/net/ent/etnc/parc_prehistorique/init/Init.java
+++ src/main/java/net/ent/etnc/parc_prehistorique/init/Init.java
@@ -21,17 +21,20 @@ public class Init implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         this.chargeZones();
+        // TODO : Ajouter toutes les entitées
     }
 
 
     private void chargeZones() {
         if (zoneService.count() != 0) return;
 
+        // TODO : Merci de faire au moins 5 entitées à chaque fois
+
         Zone zone1 = new Zone();
         zone1.setNom("Zone 1");
         zone1.setCapaciteMax(5);
-        zone1.setEnclo(TypeEnclo.MAXIMUM);
-        zone1.setEtat(EtatEnclo.ACTIF);
+        zone1.setTypeEnclo(TypeEnclo.MAXIMUM);
+        zone1.setEtatEnclo(EtatEnclo.ACTIF);
         zone1.setDescription("Description de test pour la zone 1");
 
         List.of(zone1).forEach(zoneService::create);
