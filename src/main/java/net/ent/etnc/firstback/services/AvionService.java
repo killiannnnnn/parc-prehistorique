c8363346af07a1d55f79e09a133415a52f23c37d
killiannnnnn <k.muscat@outlook.fr>
2026-08-28 08:06:00 +0200

    Delete src/main/java/net/ent/etnc/firstback directory

--- src/main/java/net/ent/etnc/firstback/services/AvionService.java
+++
@@ -1,7 +0,0 @@
-package net.ent.etnc.firstback.services;
-
-import net.ent.etnc.firstback.models.Avion;
-import net.ent.etnc.firstback.services.commons.Service;
-
-public interface AvionService extends Service<Avion, Long> {
-}
