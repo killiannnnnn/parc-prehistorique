1fd16551a7bcd065b9be67fb863d6b6000b8d6d1
killiannnnnn <k.muscat@outlook.fr>
2026-08-28 08:49:06 +0200

    Add ZoneServiceImpl

---
+++ src/main/java/net/ent/etnc/parc_prehistorique/services/impl/ZoneServiceImpl.java
@@ -0,0 +1,17 @@
+package net.ent.etnc.parc_prehistorique.services.impl;
+
+import net.ent.etnc.parc_prehistorique.models.entities.Zone;
+import net.ent.etnc.parc_prehistorique.repositories.ZoneRepository;
+import net.ent.etnc.parc_prehistorique.services.ZoneService;
+import net.ent.etnc.parc_prehistorique.services.commons.AbstractService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ZoneServiceImpl extends AbstractService<Zone, ZoneRepository> implements ZoneService {
+
+    @Autowired
+    public ZoneServiceImpl(ZoneRepository repository) {
+        super(repository);
+    }
+}
